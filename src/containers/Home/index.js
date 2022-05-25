4e73639e64792f8763c8ec9d5d5eea6e7f5087c2
Lucas Avila <lucasg.avilas@gmail.com>
2022-05-25 01:40:11 -0300

    agregado de PokemonList y cambios en el Home

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import PokemonList from '../../components/PokemonList';
 import Searcher from '../../components/Searcher';
 import './styles.css';
 
 function Home() {
   return (
-    <div className='Home'>
+    <Grid className='Home'>
       <Searcher />
-    </div>
+      <PokemonList />
+    </Grid>
   );
 }
 
